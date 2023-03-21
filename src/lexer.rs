9d1bf21e60542ca7f524ca59639c9247abca88b5
rede <rede16724@gmail.com>
2023-03-21 21:13:12 +0800

    update

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,5 +1,6 @@
 use std::ops::Range;
 
+#[derive(Debug)]
 struct Token {
     token_type: TokenType,
     range: Range<usize>,
@@ -8,12 +9,12 @@ struct Token {
 impl Token {
     fn new(token_type: TokenType, head: usize, tail: usize) -> Self {
         Self {
-            token_type: TokenType::Int,
+            token_type: token_type,
             range: head..tail,
         }
     }
 }
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 enum State {
     Init,
     String,
@@ -24,8 +25,7 @@ enum State {
     Null,
     Err,
 }
-
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 enum TokenType {
     // lexical like "string"
     String,
@@ -63,7 +63,7 @@ enum TokenType {
     // lexical "]"
     RightMiddleBracket,
 }
-
+#[derive(Debug)]
 struct Lexer {
     str: &'static str,
     head: usize,
@@ -84,39 +84,129 @@ impl Lexer {
     }
 
     fn token(&mut self) {
+        let chars = &mut self.str.chars();
         loop {
+            if self.now > self.str.len() - 1 {
+                break;
+            }
             match self.state {
-                State::Init => match &self.str.chars().next().unwrap() {
+                State::Init => match chars.next().unwrap() {
                     '{' => {
                         let t = Token::new(TokenType::LeftBigBracket, self.head, self.now);
                         self.token.push(t);
                         self.head += 1;
                         self.now += 1;
-                    },
+                    }
+                    '}' => {
+                        let t = Token::new(TokenType::RightBigBracket, self.head, self.now);
+                        self.token.push(t);
+                        self.head += 1;
+                        self.now += 1;
+                    }
+                    '[' => {
+                        let t = Token::new(TokenType::LeftMiddleBracket, self.head, self.now);
+                        self.token.push(t);
+                        self.head += 1;
+                        self.now += 1;
+                    }
+                    ']' => {
+                        let t = Token::new(TokenType::RightMiddleBracket, self.head, self.now);
+                        self.token.push(t);
+                        self.head += 1;
+                        self.now += 1;
+                    }
+                    ':' => {
+                        let t = Token::new(TokenType::Colon, self.head, self.now);
+                        self.token.push(t);
+                        self.head += 1;
+                        self.now += 1;
+                    }
+                    ',' => {
+                        let t = Token::new(TokenType::Comma, self.head, self.now);
+                        self.token.push(t);
+                        self.head += 1;
+                        self.now += 1;
+                    }
                     '0'..='9' => {
+                        println!("int");
                         self.state = State::Int;
                         self.now += 1;
-                    },
+                    }
                     '"' => {
                         self.state = State::String;
                         self.now += 1;
-                    },
+                    }
                     't' => {
                         self.state = State::True;
                         self.now += 1;
-                    },
+                    }
                     'f' => {
                         self.state = State::False;
                         self.now += 1;
-                    },
+                    }
                     'n' => {
                         self.state = State::Null;
                         self.now += 1;
-                    },
-                    _ => println!("tt"),
-                }
+                    }
+                    _ => {
+                        println!("init error");
+                        return ();
+                    }
+                },
+                State::Int => match chars.next().unwrap() {
+                    '0'..='9' => {
+                        println!("goon int");
+                        self.now += 1;
+                    }
+                    ',' => {
+                        let t = Token::new(TokenType::Int, self.head, self.now);
+                        self.token.push(t);
+                        self.head += 1;
+                        self.now += 1;
+                        let t = Token::new(TokenType::Comma, self.head, self.now);
+                        self.token.push(t);
+                        self.head += 1;
+                        self.now += 1;
+                        self.state = State::Init;
+                    }
+                    ']' => {
+                        let t = Token::new(TokenType::Int, self.head, self.now);
+                        self.token.push(t);
+                        self.head += 1;
+                        self.now += 1;
+                        let t = Token::new(TokenType::RightMiddleBracket, self.head, self.now);
+                        self.token.push(t);
+                        self.head += 1;
+                        self.now += 1;
+                        self.state = State::Init;
+                    }
+                    '}' => {
+                        let t = Token::new(TokenType::Int, self.head, self.now);
+                        self.token.push(t);
+                        self.head += 1;
+                        self.now += 1;
+                        let t = Token::new(TokenType::RightMiddleBracket, self.head, self.now);
+                        self.token.push(t);
+                        self.head += 1;
+                        self.now += 1;
+                        self.state = State::Init;
+                    }
+                    _ => {
+                        println!("state {:?}", self.state);
+                        println!("int error");
+                        return ();
+                    }
+                },
                 _ => println!("others"),
             }
         }
     }
+}
+
+#[test]
+fn test_empty_josn() {
+    let s = "{12345678}";
+    let mut lexer = Lexer::new(s);
+    lexer.token();
+    println!("{:?}", lexer.token);
 }
