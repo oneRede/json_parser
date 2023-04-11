c8507dbbaf09380afb27a581dec5794fb9b9374e
rede <rede16724@gmail.com>
2023-04-11 20:49:14 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ mod error;
 mod parser_v2;
 
 use crate::lexer::LexerError;
+
+
 fn main() {
+    const _GOTO: [i32;3] = [1,2,3];
     let _n: i32 = 3;
     println!("{:?}", LexerError::IntError);
 }
--- src/parser_v2.rs
+++ src/parser_v2.rs
@@ -1,15 +1,61 @@
-use crate::lexer::Token;
+use std::collections::HashMap;
 
+#[allow(unused)]
+#[derive(Eq, Hash, PartialEq)]
+enum TokenType {
+    // lexical like "string"
+    String,
+    // lexical like 123456
+    Int,
+    // lexical ":"
+    Colon,
+    // lexical ","
+    Comma,
+    // lexical "{"
+    LeftBigBracket,
+    // lexical "}"
+    RightBigBracket,
+    // whitespace
+    WhiteSpace,
+    //End
+    End,
+}
+
+#[allow(unused)]
+#[derive(Eq, Hash, PartialEq)]
+enum NonTerminal {
+    J,
+    A,
+    K,
+    V,
+}
+
+#[allow(unused)]
+#[derive(Debug, PartialEq)]
+enum Action {
+    Shift,
+    Reduction,
+}
+
+#[allow(unused)]
+#[derive(Eq, Hash, PartialEq)]
+enum State {
+    I0,
+    I1,
+    I2,
+    I3,
+    I4,
+    I5,
+    I6,
+    I7,
+    I8,
+    I9,
+    I10,
+    I11,
+}
+
+#[allow(unused)]
 struct Parser {
-    tokens: Vec<Token>,
+    action: HashMap<State, HashMap<TokenType, (Action, State)>>,
+    goto: HashMap<State, HashMap<NonTerminal, State>>,
 }
-
-trait JsonValue {
-    fn value(&self);
-}
-
-struct JsonObject {
-    keys: Vec<String>,
-    values: Vec<Box<dyn JsonValue>>
-}
-
